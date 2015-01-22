b8b1c1e8b8309e468eb78fad85045db6eb6c157d
Zhan <zhanli89@gmail.com>
2015-01-22 18:51:06 -0500

    update comment

--- classic_algorithms/count inversion - merge sort.py
+++ classic_algorithms/count inversion - merge sort.py
@@ -1,7 +1,9 @@
 import random
 
-# merge two sorted arrays and count split inversions
-def merge_and_countSplitInv(b, c):
+def merge_and_countsplitinv(b, c):
+    """
+    merge two sorted arrays and count split inversions
+    """
     a = []
     z = 0
     i = 0
@@ -20,14 +22,17 @@ def merge_and_countSplitInv(b, c):
     	a.extend(c[j:])
     return a, z
 
-# given an array, return sorted array and number of inversions
-def merge_sort_and_countInv(a):
+
+def merge_sort_and_countinv(a):
+    """
+    given an array, return sorted array and number of inversions
+    """
     if len(a) == 1:    # if only one element, no need to sort and no inversions
         return a, 0
-    (b, x) = merge_sort_and_countInv(a[:len(a)//2])    # sort left half and count left inner inversions
-    (c, y) = merge_sort_and_countInv(a[len(a)//2:])    # sort right half and count right inner inversions
-    (a, z) = merge_and_countSplitInv(b, c)    # merge two sorted halves and count split inversions
+    (b, x) = merge_sort_and_countinv(a[:len(a)//2])    # sort left half and count left inner inversions
+    (c, y) = merge_sort_and_countinv(a[len(a)//2:])    # sort right half and count right inner inversions
+    (a, z) = merge_and_countsplitinv(b, c)    # merge two sorted halves and count split inversions
     return a, x+y+z    # return sorted array and total inversions which equals 
                        # the sum of inner and split inversions
 
-print(merge_sort_and_countInv(random.sample(range(1, 7), 6)))
+print(merge_sort_and_countinv(random.sample(range(1, 7), 6)))
